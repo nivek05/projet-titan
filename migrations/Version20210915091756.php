7a4bdd25d6988cce65a82aebc4560fea5fa17560
nivek05 <69642810+nivek05@users.noreply.github.com>
2021-09-15 11:28:16 +0200

    Add files via upload

---
+++ migrations/Version20210915091756.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210915091756 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE users_registration (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(50) NOT NULL, name VARCHAR(50) NOT NULL, email VARCHAR(50) NOT NULL, registration_date DATE NOT NULL, is_accepted TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_31EC81ABE7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE users_registration');
+    }
+}
